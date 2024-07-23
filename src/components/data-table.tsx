781a3a9cac11494444b43a3a0d9ce74f448499f7
Ankit singh <as1230641@gmail.com>
2024-07-24 03:40:27 +0530

    .

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -162,25 +162,6 @@ export function DataTable<TData, TValue>({
         </Table>
       </div>
 
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
-      </div>
-
       {selectedCharacter && (
         <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
           <SheetContent>
